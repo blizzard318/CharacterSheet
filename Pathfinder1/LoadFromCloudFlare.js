e4ce4b6b7c951bd7a2ab516a80c6e512c99fbf94
Andrew Chen <anew318@hotmail.com>
2023-04-27 17:20:33 +0800

    does this do anything

--- Pathfinder1/LoadFromCloudFlare.js
+++ Pathfinder1/LoadFromCloudFlare.js
@@ -1,23 +1,16 @@
 async function LoadFromCloudFlare () {
-	console.log("Why won't you work");
 	const response = await fetch("../api/Pathfinder1/Andrew/1");
 	const json = await response.json();
-	console.log(json);
+	const character = JSON.parse(json);
 	
-	/*const files = document.getElementById("charsheet").files;
-	const text = await files[0].text();
-	const json = JSON.parse(text);
+	document.getElementsByName("CharacterName")[0].value = character.name;
+	document.getElementsByName("PlayerName")[0].value = character.player;
 	
-	let character = { };
+	document.getElementsByName("Class")[0].value = character.class;
 	
-	character.name = document.getElementsByName("CharacterName")[0].value;
-	character.player = document.getElementsByName("PlayerName")[0].value;
-	
-	character.class = document.getElementsByName("Class")[0].value;
-	
-	character.race = document.getElementsByName("Race")[0].value;
-	character.alignment = document.getElementsByName("Alignment")[0].value;
-	character.gender = document.getElementsByName("Gender")[0].value;
-	character.size = document.getElementsByName("Size")[0].value;
-	character.deity = document.getElementsByName("Deity")[0].value;*/
+	document.getElementsByName("Race")[0].value = character.race;
+	document.getElementsByName("Alignment")[0].value = character.alignment;
+	document.getElementsByName("Gender")[0].value = character.gender;
+	document.getElementsByName("Size")[0].value = character.size;
+	document.getElementsByName("Deity")[0].value = character.deity;
 }
