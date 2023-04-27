862f07a34562bf407b3162de643be805de8449d2
Andrew Chen <anew318@hotmail.com>
2023-04-27 17:25:43 +0800

    does it work this way

--- Pathfinder1/LoadFromCloudFlare.js
+++ Pathfinder1/LoadFromCloudFlare.js
@@ -1,8 +1,6 @@
 async function LoadFromCloudFlare () {
 	const response = await fetch("../api/Pathfinder1/Andrew/1");
-	const json = await response.json();
-	console.log(json);
-	const character = JSON.parse(json);
+	const character = await response.json();
 	
 	document.getElementsByName("CharacterName")[0].value = character.name;
 	document.getElementsByName("PlayerName")[0].value = character.player;
