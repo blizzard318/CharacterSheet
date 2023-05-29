b7d80d4806a4ba7929a78fcf1515dd2e9cdb0c2a
Andrew Chen <anew318@hotmail.com>
2023-05-29 15:33:22 +0800

    attempt

--- functions/api/[[params]].js
+++ functions/api/[[params]].js
@@ -52,7 +52,7 @@ export async function onRequestGet (context) { //Get list or single character
 			const character = await KVList.get(fullkey,{type:"json"});
 			retval.push(character);
 		}
-		return new Response(JSON.stringify(retval));
+		return new Response(retval);
 	} else { //Single character
 		const key = PlayerName + "/" + CharacterIndex;
 		const character = await KVList.get(key);
