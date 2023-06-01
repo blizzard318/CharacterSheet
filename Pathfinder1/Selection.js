bcfa94cd860a5fc4955815e41e5b719f85779f06
Andrew Chen <anew318@hotmail.com>
2023-06-01 15:20:54 +0800

    some page nav fixes

--- Pathfinder1/Selection.js
+++ Pathfinder1/Selection.js
@@ -11,20 +11,18 @@ async function ShowPart2(name) {
 	const list = document.getElementById("CharacterList");
 	list.innerHTML = ""; //Clear previous elements
 	
-	if (name != "Stranger") {
-		const response = await fetch("../api/Pathfinder1/" + name);
-		const characters = await response.json();
+	const response = await fetch("../api/Pathfinder1/" + name);
+	const characters = await response.json();
+	
+	for (let character of characters) {
+		let button = document.createElement("button");
+		button.innerText = "Name: " + character.name + " Class: " + character.class;
 		
-		for (let character of characters) {
-			let button = document.createElement("button");
-			button.innerText = "Name: " + character.name + " Class: " + character.class;
-			
-			let a = document.createElement("a");
-			a.setAttribute("href","./?key=" + character.key);
-			a.appendChild(button);
-			
-			list.appendChild(document.createElement("br"));
-			list.appendChild(a);
-		}
+		let a = document.createElement("a");
+		a.setAttribute("href","./?key=" + character.key);
+		a.appendChild(button);
+		
+		list.appendChild(document.createElement("br"));
+		list.appendChild(a);
 	}
 }
