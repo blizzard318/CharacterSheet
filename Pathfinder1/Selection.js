578c2cc638c1ba37af94cabd8fce14351fd12fdb
Andrew Chen <anew318@hotmail.com>
2023-06-01 14:52:50 +0800

    button fixes

--- Pathfinder1/Selection.js
+++ Pathfinder1/Selection.js
@@ -1,9 +1,15 @@
 async function ShowPart2(name) {
 	document.getElementById("Part2").style.display = "block";
 	document.getElementById("Name").innerHTML = name;
+	
+	const buttons = document.getElementsByTagName("button");
+	for (const button of buttons) {
+	  button.disabled = false;
+	} //Re-enable all buttons, disable clicked button
+	document.getElementById(name).disabled = true;
   
 	const list = document.getElementById("CharacterList");
-	list.innerHTML = "";
+	list.innerHTML = ""; //Clear previous elements
 	
 	if (name != "Stranger") {
 		const response = await fetch("../api/Pathfinder1/" + name);
