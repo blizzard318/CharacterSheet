bf56e8db45065d88c312e8734407fff12ebf4958
Andrew Chen <anew318@hotmail.com>
2023-06-01 13:57:43 +0800

    clear the previous char first.

--- Pathfinder1/Selection.js
+++ Pathfinder1/Selection.js
@@ -1,13 +1,14 @@
 async function ShowPart2(name) {
-	const Part2 = document.getElementById("Part2");
-	Part2.style.display = "block";
+	document.getElementById("Part2").style.display = "block";
 	document.getElementById("Name").innerHTML = name;
   
+	const list = document.getElementById("CharacterList");
+	list.innerHTML = "";
+	
 	if (name != "Stranger") {
 		const response = await fetch("../api/Pathfinder1/" + name);
 		const characters = await response.json();
 		
-		const list = document.getElementById("CharacterList");
 		for (let character of characters) {
 			let button = document.createElement("button");
 			button.innerText = "Name: " + character.name + " Class: " + character.class;
@@ -16,8 +17,8 @@ async function ShowPart2(name) {
 			a.setAttribute("href","./?key=" + character.key);
 			a.appendChild(button);
 			
-			Part2.appendChild(document.createElement("br"));
-			Part2.appendChild(a);
+			list.appendChild(document.createElement("br"));
+			list.appendChild(a);
 		}
 	}
 }
