215553da54fca5123558f67d99ae79b152210971
Andrew Chen <anew318@hotmail.com>
2023-06-07 17:48:47 +0800

    its not done

--- Pathfinder1/Selection.js
+++ Pathfinder1/Selection.js
@@ -26,7 +26,26 @@ async function ShowPart2(name) {
 		let button = document.createElement("button");
 		button.innerText = "Name: " + character.name + "\r\nClass: " + character.class;
 		
+		let delete_button = document.createElement("button");
+		delete_button.innerText = "Delete";
+		button.appendChild(delete_button);
+		
 		let a = document.createElement("a");
+		a.setAttribute("href","./?key=" + character.key);
+		
+		const url = "../api/Pathfinder1/" + character.player;
+		fetch(url,{
+			method: "POST", //THIS IS POST, NEW CHARA. DON'T USE PUT.
+			headers: { 
+			  'Accept': 'application/json',
+			  'Content-Type': 'application/json'
+			},
+			body: JSON.stringify(character)
+		});
+		
+		a.appendChild(delete_button);
+		
+		a = document.createElement("a");
 		a.setAttribute("href","./?key=" + character.key);
 		a.appendChild(button);
 		
