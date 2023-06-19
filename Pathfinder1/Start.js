787eaa701125052cfbb778f0c4578844177d4f3e
Andrew Chen <anew318@hotmail.com>
2023-06-19 16:55:34 +0800

    pls work

--- Pathfinder1/Start.js
+++ Pathfinder1/Start.js
@@ -8,11 +8,87 @@ async function LoadFromCloudFlare (key) {
 	
 	document.getElementById("Class").value = character.class;
 	
-	document.getElementById("Race").value = character.race;
-	document.getElementById("Alignment").value = character.align;
-	document.getElementById("Gender").value = character.gender;
-	document.getElementById("Size").value = character.size;
-	document.getElementById("Deity").value = character.deity;
+	document.getElementById("Race").value 	   	= character.race;
+	document.getElementById("Alignment").value 	= character.align;
+	document.getElementById("Gender").value 	= character.gender;
+	document.getElementById("Size").value  		= character.size;
+	document.getElementById("Deity").value 		= character.deity;
+	
+	document.getElementById("str").value 	   = character.str;
+	 document.getElementById("str-temp").value = character.str_t;
+	 document.getElementById("dex").value      = character.dex;
+	 document.getElementById("dex-temp").value = character.dex_t;
+	 document.getElementById("con").value      = character.con;
+	 document.getElementById("con-temp").value = character.con_t;
+	
+	document.getElementById("int").value      = character.int;
+	document.getElementById("int-temp").value = character.int_t;
+	document.getElementById("wis").value      = character.wis;
+	document.getElementById("wis-temp").value = character.wis_t;
+	document.getElementById("cha").value      = character.cha;
+	document.getElementById("cha-temp").value = character.cha_t;
+	
+	document.getElementById("Total-AC").value      = character.ac_total;
+	document.getElementById("Armor-Bonus").value   = character.ac_bonus;
+	document.getElementById("Shield-Bonus").value  = character.ac_shield;
+	document.getElementById("AC-Size").value       = character.ac_size;
+	document.getElementById("Natural-Armor").value = character.ac_nat;
+	document.getElementById("Deflection").value    = character.ac_deflec;
+	document.getElementById("AC-Misc").value       = character.ac_misc;
+	
+	document.getElementById("Touch-AC").value    = character.ac_t;
+	document.getElementById("FlatFoot-AC").value = character.ac_ff;
+	document.getElementById("Other-AC").value    = character.ac_other;
+	
+	document.getElementById("Current-HP").value 	= character.hp_current;
+	document.getElementById("Temp-HP").value 		= character.hp_temp;
+	document.getElementById("Non-Lethal-Dmg").value = character.hp_nonlethal;
+	document.getElementById("Max-HP").value 		= character.hp_max;
+	
+	document.getElementById("Damage-Reduction").value  = character.dmg_reduc;
+	 document.getElementById("Spell-Resistance").value = character.spell_resist;
+	
+	document.getElementById("Fortitude-Total").value = character.fort_total;
+	document.getElementById("Fortitude-Base").value  = character.fort_base;
+	document.getElementById("Fortitude-Item").value  = character.fort_item;
+	document.getElementById("Fortitude-Misc").value  = character.fort_misc;
+	document.getElementById("Fortitude-Notes").value = character.fort_note;
+	
+	document.getElementById("Reflex-Total").value = character.ref_total;
+	document.getElementById("Reflex-Base").value  = character.ref_base;
+	document.getElementById("Reflex-Item").value  = character.ref_item;
+	document.getElementById("Reflex-Misc").value  = character.ref_misc;
+	document.getElementById("Reflex-Notes").value = character.ref_note;
+	
+	document.getElementById("Will-Total").value = character.will_total;
+	document.getElementById("Will-Base").value  = character.will_base;
+	document.getElementById("Will-Item").value  = character.will_item;
+	document.getElementById("Will-Misc").value  = character.will_misc;
+	document.getElementById("Will-Notes").value = character.will_note;
+	
+	document.getElementById("Resistances").value = character.resist;
+	document.getElementById("Immunities").value  = character.immune;
+	
+	document.getElementById("Total-CMD").value = character.cmd_total;
+	document.getElementById("Size-CMD").value  = character.cmd_size;
+	document.getElementById("Misc-CMD").value  = character.cmd_misc;
+	
+	document.getElementById("Init-Total").value = character.init_total;
+	document.getElementById("Misc-Init").value = character.init_misc;
+	
+	document.getElementById("BAB").value = character.bab;
+	
+	document.getElementById("Base-Speed").value   = character.spd_base;
+	document.getElementById("Armor-Speed").value  = character.spd_amr;
+	document.getElementById("Fly-Speed").value    = character.spd_fly;
+	document.getElementById("Swim-Speed").value   = character.spd_swm;
+	document.getElementById("Climb-Speed").value  = character.spd_clb;
+	document.getElementById("Burrow-Speed").value = character.spd_brw;
+	document.getElementById("Speed-Notes").value  = character.spd_nts;
+	
+	document.getElementById("Total-CMB").value = character.cmb_total;
+	document.getElementById("Size-CMB").value  = character.cmb_size;
+	document.getElementById("Misc-CMB").value  = character.cmb_misc;
 }
 
 function CheckURL () {
--- Pathfinder1/Update.js
+++ Pathfinder1/Update.js
@@ -2,13 +2,14 @@
 function ModifyAbility (name) {
 	SaveToCloudFlare();
 	const abi = parseInt(document.getElementById(name).value);
-	if (typeof abi != 'number') return; //Invalid number
+	if (isNaN(abi)) return; //Invalid number
 	const mod = Math.floor((abi - 10)/2);
 	
 	if (!name.includes("temp")) { //If original ability score
 		document.getElementById(name + "-mod").value = mod;
+		
 		const temp_abi = parseInt(document.getElementById(name + "-temp").value);
-		if (temp_abi != null && temp_abi != "" && temp_abi == typeof temp_abi == 'number') return;
+		if (!isNaN(temp_abi)) return;
 	}
 	
 	const elements = document.getElementsByClassName(name + "-temp-mod");
