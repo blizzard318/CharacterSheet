b7f3c618a89508d9f33eefe12651c16cadfd4a31
Andrew Chen <anew318@hotmail.com>
2023-06-19 16:01:27 +0800

    let's try this

--- Pathfinder1/Update.js
+++ Pathfinder1/Update.js
@@ -1,15 +1,13 @@
 //This code runs like an update loop.
 function ModifyAbility (name) {
 	SaveToCloudFlare();
-	const abi = document.getElementById(name).value;
-	document.getElementById("str-mod").value = abi;
+	const abi = parseInt(document.getElementById(name).value);
 	if (typeof abi != 'number') return; //Invalid number
-	document.getElementById("str-mod").value = abi * 2;
 	const mod = Math.floor((abi - 10)/2);
 	
 	if (!name.includes("temp")) { //If original ability score
 		document.getElementById(name + "-mod").value = mod;
-		const temp_abi = document.getElementById(name + "-temp").value;
+		const temp_abi = parseInt(document.getElementById(name + "-temp").value);
 		if (typeof temp_abi == 'number') return; //If there's a valid number, return.
 	}
 	
