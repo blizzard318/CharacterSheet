fa456430ba1d0548cab8d6d81a2936bf8f7aacc8
Andrew Chen <anew318@hotmail.com>
2023-06-20 14:01:21 +0800

    why

--- Pathfinder1/Update.js
+++ Pathfinder1/Update.js
@@ -2,14 +2,13 @@
 function ModifyAbility (name) {
 	SaveToCloudFlare();
 	const abi = parseInt(document.getElementById(name).value);
-	if (isNaN(abi)) return; //Invalid number
 	const mod = Math.floor((abi - 10)/2);
 	
 	if (!name.includes("temp")) { //If original ability score
 		document.getElementById(name + "-mod").value = mod;
 		
-		const temp_abi = parseInt(document.getElementById(name + "-temp").value);
-		if (!isNaN(temp_abi)) return;
+		const temp_abi = document.getElementById(name + "-temp").value;
+		if (temp_abi != "" && temp_abi != null) return;
 	}
 	
 	const elements = document.getElementsByClassName(name + "-temp-mod");
