1562359a17809195cee0ed0f2ed85a70ae3bd1b3
Andrew Chen <anew318@hotmail.com>
2023-07-03 15:53:22 +0800

    woop

--- functions/feed/rss.xml.js
+++ functions/feed/rss.xml.js
@@ -9,20 +9,18 @@ export function onRequestGet(context) {
 	let text = '<?xml version="1.0" encoding="UTF-8"?>';
 	text += '<rss version="2.0">';
 	text += '<channel>';
-	text += '<title>RSS Title</title>';
-	text += '<description>This is an example of an RSS feed</description>';
-	text += '<link>http://www.example.com/main.html</link>';
-	text += '<copyright>2020 Example.com All rights reserved</copyright>';
-	text += '<lastBuildDate>Mon, 6 Sep 2010 00:01:00 +0000</lastBuildDate>';
-	text += '<pubDate>Sun, 6 Sep 2009 16:20:00 +0000</pubDate>';
-	text += '<ttl>1800</ttl>';
-
+	text += '<title>IceCubez Character Sheets</title>';
+	text += '<description>This is to track when characters are updated.</description>';
+	text += '<language>en-us</language>';
+	text += '<link>https://charactersheet.pages.dev/</link>';
+	text += '<lastBuildDate>Mon, 3 Jul 2023 03:30:00 SGT</lastBuildDate>';
+	text += '<managingEditor>anew318@hotmail.com</managingEditor>';
+	text += '<webMaster>anew318@hotmail.com</webMaster>';
+	
 	text += '<item>';
-	text += ' <title>Example entry</title>';
-	text += ' <description>Here is some text containing an interesting description.</description>';
-	text += ' <link>http://www.example.com/blog/post/1</link>';
-	text += ' <guid isPermaLink="false">7bd204c6-1655-4c27-aeee-53f933c5395f</guid>';
-	text += ' <pubDate>Sun, 6 Sep 2009 16:20:00 +0000</pubDate>';
+	text += ' <title>First entry</title>';
+	text += ' <description>This is more an experiment than something useful.</description>';
+	text += ' <pubDate>Mon, 3 Jul 2023 03:30:00 SGT</pubDate>';
 	text += '</item>';
 
 	text += '</channel>';
