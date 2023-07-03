b15ce952076671971ac328bad5781749ab63e73e
Andrew Chen <anew318@hotmail.com>
2023-07-03 16:08:48 +0800

    last change for now

--- functions/feed/rss.xml.js
+++ functions/feed/rss.xml.js
@@ -6,26 +6,29 @@ function GetKV (ctx) {
 }
 
 export function onRequestGet(context) {
-	let text = '<?xml version="1.0" encoding="UTF-8"?>';
-	text += '<rss version="2.0">';
-	text += '<channel>';
-	text += '<title>IceCubez Character Sheets</title>';
-	text += '<description>This is to track when characters are updated.</description>';
-	text += '<language>en-us</language>';
-	text += '<link>https://charactersheet.pages.dev/</link>';
-	text += '<lastBuildDate>Mon, 3 Jul 2023 03:30:00 SGT</lastBuildDate>';
-	text += '<managingEditor>anew318@hotmail.com</managingEditor>';
-	text += '<webMaster>anew318@hotmail.com</webMaster>';
+	let text = '<?xml version="1.0" encoding="UTF-8"?>\n';
+	text += '<rss version="2.0">\n<channel>\n';
 	
-	text += '<item>';
-	text += ' <title>First entry</title>';
-	text += ' <description>This is more an experiment than something useful.</description>';
-	text += ' <pubDate>Mon, 3 Jul 2023 03:30:00 SGT</pubDate>';
-	text += '</item>';
+	text += '<title>IceCubez Character Sheets</title>\n';
+	text += '<description>This is to track when characters are updated.</description>\n';
+	text += '<language>en-us</language>\n';
+	text += '<link>https://charactersheet.pages.dev/</link>\n';
+	text += '<lastBuildDate>Mon, 03 Jul 2023 03:30:00 SGT</lastBuildDate>\n';
+	text += '<managingEditor>anew318@hotmail.com (Andrew)</managingEditor>\n';
+	text += '<webMaster>anew318@hotmail.com (Andrew)</webMaster>\n';
+	
+	text += '<item>\n';
+	text += ' <title>First entry</title>\n';
+	text += ' <description>This is more an experiment than something useful.</description>\n';
+	text += ' <pubDate>Mon, 03 Jul 2023 03:30:00 SGT</pubDate>\n';
+	text += '</item>\n';
 
-	text += '</channel>';
-	text += '</rss>';
+	text += '</channel>\n</rss>\n';
 	
-	return new Response(text, {status: 200})
+	return new Response(text, {
+		status: 200,
+		headers: {
+			"content-type": "application/rss+xml;charset=UTF-8",
+		},
+	});
 }
-
